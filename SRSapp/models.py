9359ec9b08bffa519a90c71f32a6458e59c7b3ff
Kimomwe2003 <lidyakimomwe@gmail.com>
2024-12-23 09:21:46 +0300

    sec

--- SRSapp/models.py
+++ SRSapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
-# Create your models here.
+# Create your models hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     address = models.CharField(max_length=15, default="Jumbi")
